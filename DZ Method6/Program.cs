be75022d977bf75b0ff2c5c2b5ac2a4ab5bf4a53
ZewsSD <76225337+ZewsSD@users.noreply.github.com>
2021-03-21 08:11:07 +0300

    Add files via upload

---
+++ DZ Method6/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ_Method6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+
+            CutWords(ref str);
+            Console.WriteLine(str);
+
+            Console.ReadKey();
+        }
+
+        public static string CutWords(ref string text)
+        {
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (char.IsDigit(text[i]) != true)  
+                {
+                    text = text.Remove(i, 1);
+                    i--;
+                }
+            }
+
+            return text;
+        }
+    }
+}
---
+++ DZ Method8/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ_Method8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+
+            QuadraticEquation(a,b,c);
+
+            Console.ReadKey();
+        }
+
+        public static void QuadraticEquation(int a, int b, int c)
+        {
+            double D = (b * b) - (4 * a * c);
+            Console.WriteLine("Дескриминант: " + D);
+            double SqrtD = Math.Sqrt(D);
+
+            double x1 = (-b + SqrtD)/(2*a);
+            double x2 = (-b - SqrtD) / (2 * a);
+
+            Console.WriteLine("x1: " + x1);
+            Console.WriteLine("x2: " + x2);
+        }
+    }
+}
